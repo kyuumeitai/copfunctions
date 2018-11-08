bf6dbfc487a485687351d328b533cdf50c360a41
Álex Acuña Viera <kyuumeitai@gmail.com>
2018-11-08 10:43:21 -0300

    svg support

--- copfunctions.php
+++ copfunctions.php
@@ -9,9 +9,18 @@
     License: GPLv3
     */
 
-add_filter( 'shortcode_atts_gallery', 'cops_gallery_atts', 10, 3 );
-
 function cops_gallery_atts( $output, $pairs, $atts ) {
         $output['size'] = 'large';
         return $output;
 };
+
+add_filter( 'shortcode_atts_gallery', 'cops_gallery_atts', 10, 3 );
+
+function add_file_types_to_uploads($file_types){
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+    return $file_types;
+}
+
+add_action('upload_mimes', 'add_file_types_to_uploads');
